bd63d354cbe9b000f8850f5d38dacf924b4a2244
Tausifqureshi <tausifqureshi504@gmail.com>
2025-05-20 21:50:49 +0530

    Mini-Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { version } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-900 bg-yellow-300 p-5 ">
+        App {version}
+      </h1>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
