bb5ff4600082bf5410d9cdcfdaa551a9c852d2b7
Tausifqureshi <tausifqureshi504@gmail.com>
2025-06-25 00:14:59 +0530

    Lom

--- src/routerExample/page/ProtectedRoute.jsx
+++ src/routerExample/page/ProtectedRoute.jsx
@@ -2,8 +2,7 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../contexAPI/AuthProvider ";
 
 const ProtectedRoute = () => {
-    const location = useLocation();
-
+    const location = useLocation(); 
 
   const { user } = useAuth();
   return user ? <Outlet /> : <Navigate to="/login" state={{ from: location.pathname }} />;
