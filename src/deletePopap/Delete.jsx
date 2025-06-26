ebee6903c56a9f751dd234c620c1632707a5d128
Tausifqureshi <tausifqureshi504@gmail.com>
2025-06-26 10:25:31 +0530

    Order-Base

---
+++ src/deletePopap/Delete.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+function Delete({ onDelete, onClose }) {
+//   const handleDelete = () => {
+//     onDelete();
+//     onClose();
+//   };
+
+  return (
+    <div className="delete-popup">
+      <h2>Are you sure you want to delete this item?</h2>
+      {/* <button onClick={handleDelete}>Yes, delete</button>
+      <button onClick={onClose}>No, cancel</button> */}
+    </div>
+  );
+}
+export default Delete;
