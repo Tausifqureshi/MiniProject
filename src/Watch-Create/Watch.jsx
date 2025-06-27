4157696b9d53d19facfcc342c7cebd4f200e9cb0
Tausifqureshi <tausifqureshi504@gmail.com>
2025-06-27 09:18:33 +0530

    Delete Popup

--- src/Watch-Create/Watch.jsx
+++ src/Watch-Create/Watch.jsx
@@ -1,21 +1,21 @@
-// import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect} from 'react'
 
-// function Watch({color}) {
-//   const [time , setTime] = useState(0);
+function Watch({color}) {
+  const [time , setTime] = useState(0);
    
-//   useEffect(()=>{
-//     const interval = setInterval(() => {
-//       setTime(new Date().toLocaleTimeString());
-//     }, 1000);
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
 
-//     return () => clearInterval(interval); // Cleanup on unmount
-//   }, []);
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
 
-//   return (
-//     <div>
-//       <h1 style={{color: color, backgroundColor: "#f0f0f0", padding: "10px", borderRadius: "5px"}}>Time: {time}</h1>
-//       {/* <button onClick={() => setTime(time + 1)}>Increment</button> */}
-//     </div>
-//   )
-// }
-// export default Watch
+  return (
+    <div>
+      <h1 style={{color: color, backgroundColor: "#f0f0f0", padding: "10px", borderRadius: "5px"}}>Time: {time}</h1>
+      {/* <button onClick={() => setTime(time + 1)}>Increment</button> */}
+    </div>
+  )
+}
+export default Watch
