d6632de469bcca45e79ceea6f7408213e74d9b9f
Tausifqureshi <tausifqureshi504@gmail.com>
2025-07-11 09:51:49 +0530

    Updste

--- src/AddToCart/AddTocart.jsx
+++ src/AddToCart/AddTocart.jsx
@@ -1,104 +1,34 @@
-{/* <h1 align="center">Hey 👋, I'm Tausif Qureshi</h1>
 
-<p align="center">
-  <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=500&size=22&pause=1000&center=true&vCenter=true&color=F97316&width=500&lines=React+Developer+%7C+MERN+Stack+Enthusiast;Front-End+Web+Engineer+%7C+UI+Specialist;Always+Learning+%7C+Open+to+Collaboration" alt="Typing SVG" />
-</p>
 
----
 
-## 🧑‍💻 About Me
 
-<table>
-<tr>
 
-<!-- LEFT SIDE -->
-<td width="55%" valign="top">
 
-<h3>🚀 Web Engineer | React Frontend Developer | MERN Stack Explorer</h3>
 
-- 🔥 I craft dynamic, responsive UIs using <b>React.js</b>  
-- 🎯 Currently diving deeper into <b>Node.js, Express & MongoDB</b>  
-- 💼 Strong foundation in <b>HTML5, CSS3, JavaScript, Tailwind, Redux</b>  
-- 🛠️ Passionate about <b>performance, accessibility & UI/UX</b>  
-- 🤝 Actively contributing to <b>open source & collaborative projects</b>  
-- 📫 Email: [tausifqureshi504@gmail.com](mailto:tausifqureshi504@gmail.com)  
-- 🌐 Portfolio: [tausifporfolio-website.netlify.app](https://tausifporfolio-website.netlify.app)
 
-</td>
 
-<!-- RIGHT SIDE -->
-<td width="45%">
-  <img src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" alt="dev-gif" width="100%" />
-</td>
 
-</tr>
-</table>
 
----
 
-## 🚀 Tech Stack
 
-<p align="center">
-  <img src="https://skillicons.dev/icons?i=html,css,js,react,redux,tailwind,bootstrap,materialui,nodejs,express,mongodb" />
-</p>
 
----
 
-## 🧰 Tools & Platforms
 
-<p align="center">
-  <img src="https://skillicons.dev/icons?i=vscode,github,git,postman,netlify,heroku,codepen,bash" />
-</p>
 
----
 
-## 🌐 Connect with Me
 
-<p align="center">
-  <a href="https://linkedin.com/in/tausif-qureshi" target="_blank"><img src="https://skillicons.dev/icons?i=linkedin" height="40"/></a>&nbsp;
-  <a href="mailto:tausifqureshi504@gmail.com"><img src="https://cdn-icons-png.flaticon.com/128/732/732200.png" height="40"/></a>&nbsp;
-  <a href="https://wa.me/8429097693" target="_blank"><img src="https://cdn-icons-png.flaticon.com/128/733/733585.png" height="40"/></a>&nbsp;
-  <a href="https://twitter.com/Tausif_qu16823" target="_blank"><img src="https://skillicons.dev/icons?i=twitter" height="40"/></a>
-</p>
 
----
 
-## 📈 GitHub Stats
 
-<p align="center">
-  <img src="https://github-readme-stats.vercel.app/api?username=Tausifqureshi&show_icons=true&theme=tokyonight&hide_border=true" width="48%" />
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Tausifqureshi&layout=compact&theme=tokyonight&hide_border=true" width="48%" />
-</p>
 
----
 
-## 🔥 GitHub Streak
 
-<p align="center">
-  <img src="https://github-readme-streak-stats.herokuapp.com/?user=Tausifqureshi&theme=github-dark-blue&hide_border=true" />
-</p>
 
----
 
-## 🏆 GitHub Achievements
 
-<p align="center">
-  <img src="https://github-profile-trophy.vercel.app/?username=Tausifqureshi&theme=flat&no-frame=true&margin-w=5&row=1&column=6" />
-</p>
 
----
 
-## 🧠 Developer Wisdom
 
-<p align="center">
-  <i>"Clean code always looks like it was written by someone who cares." – <b>Robert C. Martin</b></i>
-</p>
 
----
 
-<h3 align="center">⭐ Show some love by starring my repositories!</h3>
 
-<p align="center">
-  <img src="https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif" width="100" />
-</p>
- */}
