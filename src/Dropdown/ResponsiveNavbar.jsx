f884d5f941eb6f4b5e30c386c97bcdd3bb852b91
Tausifqureshi <tausifqureshi504@gmail.com>
2026-01-06 10:51:07 +0530

    Update noe bottom compoents

--- src/Dropdown/ResponsiveNavbar.jsx
+++ src/Dropdown/ResponsiveNavbar.jsx
@@ -118,7 +118,7 @@ export default ResponsiveNavbar;
 
 
 
-
+  
 // I worked as a freelance Frontend Developer, building responsive and user-friendly web applications using React.js, Next.js, and TypeScript. My responsibilities included collaborating with clients to convert UI/UX designs into reusable components, integrating REST APIs, and ensuring the delivery of production-ready applications with optimized performance and cross-browser compatibility. I developed responsive web applications, implemented component-based UI architecture, and integrated dynamic data rendering through REST APIs. Additionally, I used Redux and Context API for state management, improved UI performance, responsiveness, and accessibility, and deployed applications on Vercel and Netlify. I also maintained clean and maintainable code using Git and GitHub, while continuously working with clients to enhance features and update requirements. My tools and technologies included React.js, Next.js, TypeScript, JavaScript (ES6+), Tailwind CSS, Material-UI, Redux, Git, GitHub, Firebase, Vercel, and Netlify.
 
 
