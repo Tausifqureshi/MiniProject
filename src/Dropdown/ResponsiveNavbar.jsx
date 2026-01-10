6b455cb4d94100ae3177e8a138e6e867bc9a90c4
Tausifqureshi <tausifqureshi504@gmail.com>
2026-01-10 10:41:08 +0530

    Updtae now responovs

--- src/Dropdown/ResponsiveNavbar.jsx
+++ src/Dropdown/ResponsiveNavbar.jsx
@@ -133,4 +133,4 @@ export default ResponsiveNavbar;
 
 // Resume headline Naukri ki change 
 // Frontend Engineer skilled in HTML5, CSS3, JavaScript (ES6+), TypeScript, React.js, Next.js, Firebase, Node.js, Redux, React Router, Tailwind, Bootstrap, Material-UI, Git, GitHub, REST APIs.
-// LinkedIn: https://www.linkedin.com/in/tausif-qureshi/
+// LinkedIn: https://www.linkedin.com/in/tausif-qureshi/ 
